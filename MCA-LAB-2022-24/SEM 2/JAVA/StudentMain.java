81d3bf1978ff00c52384bff71f944f6739c91a5b
Deepak KS <106476013+Deepak-ks-git@users.noreply.github.com>
2023-06-12 13:10:36 +0530

    Add files via upload

---
+++ MCA-LAB-2022-24/SEM 2/JAVA/StudentMain.java
@@ -0,0 +1,47 @@
+class Student {
+    int rollno;
+    String name;
+    int mark1, mark2;
+
+    void getDetails(int rn, String name) {
+        this.rollno = rn;
+        this.name = name;
+    }
+
+    void getMarks(int m1, int m2) {
+        this.mark1 = m1;
+        this.mark2 = m2;
+    }
+}
+
+interface Sports {
+    final int gracemark = 10;
+
+    void getGraceMark();
+}
+
+class Result extends Student implements Sports {
+    void getGraceMark() {
+        System.out.println("Grace mark awarded: " + gracemark);
+    }
+
+    void getResult() {
+        System.out.println("ROLL NO: " + rollno);
+        System.out.println("NAME: " + name);
+        System.out.println("MARK 1: " + mark1);
+        System.out.println("MARK 2: " + mark2);
+        System.out.println("SPORTS MARK: " + gracemark);
+        System.out.println("TOTAL MARK (Out of 100): " + (gracemark + mark1 + mark2));
+    }
+}
+
+class StudentMain {
+    public static void main(String args[]) {
+        Result r = new Result();
+        r.getDetails(10, "deepak");
+        r.getMarks(40, 40);
+        r.getGraceMark();
+        r.getResult();
+    }
+}
+
