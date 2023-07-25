988898808694389c43069ef4466d284fcd651b1e
Deepak KS <106476013+Deepak-ks-git@users.noreply.github.com>
2023-07-25 07:46:40 +0530

    Update LoginMain.java

--- MCA-LAB-2022-24/SEM 2/JAVA/CO4/LoginMain.java
+++ MCA-LAB-2022-24/SEM 2/JAVA/CO4/LoginMain.java
@@ -1,37 +1,33 @@
 import java.util.*;
-class Validate extends Exception{
-    Validate(String s){
+
+class Validate extends Exception {
+    Validate(String s) {
         super(s);
     }
 }
 
-
 public class LoginMain {
-    public static void main(String args[])
-    {
-        String uname,passwd;
-        try{
-        Scanner sc=new Scanner(System.in);
-        System.out.println("enter your user name :");
-        uname=sc.next();
-        System.out.println("enter your password:");
-        passwd=sc.next();
-        check(uname,passwd);
+    public static void main(String args[]) {
+        String uname, passwd;
+        try {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("enter your user name :");
+            uname = sc.next();
+            System.out.println("enter your password:");
+            passwd = sc.next();
+            check(uname, passwd);
 
+        } catch (Validate v) {
+            System.out.println("Invalid credentials" + v);
         }
-        catch(Validate v)
-        {
-            System.out.println("Invalid credentials"+v);
-        }
-
 
     }
 
-    static void check(String u,String p) throws Validate{
-        if(u.equals("deepak") && p.equals("Deepak@123"))
+    static void check(String u, String p) throws Validate {
+        if (u.equals("deepak") && p.equals("Deepak@123"))
             System.out.println("Login success");
         else
             throw new Validate("Incorrect password or username");
     }
-    
+
 }
