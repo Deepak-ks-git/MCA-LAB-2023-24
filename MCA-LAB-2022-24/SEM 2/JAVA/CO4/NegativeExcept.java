e0582db96cb273aceabd21bb8608d822fb82de8e
Deepak KS <106476013+Deepak-ks-git@users.noreply.github.com>
2023-07-25 07:44:48 +0530

    Add files via upload

---
+++ MCA-LAB-2022-24/SEM 2/JAVA/CO4/NegativeExcept.java
@@ -0,0 +1,41 @@
+import java.util.*;
+class NegativeNumber extends Exception{
+    NegativeNumber(String s){
+        super(s);
+
+    }
+}
+
+
+public class NegativeExcept {
+
+    public static void main(String args[]){
+
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter the number of positive integers (n): ");
+        int n = sc.nextInt();
+        int count=0;
+        int sum=0;
+        try{
+            for(int i=0;i<n;i++){
+            System.out.print("Enter a positive number");
+            int  num=sc.nextInt();
+            if(num<0){
+                throw new NegativeNumber("Negative number are not allowed");
+            }
+            sum+=num;
+            count++;
+            }
+            double avg=sum/count;
+            System.out.println("Average is :"+avg);
+        }
+        catch (NegativeNumber ne){
+
+            System.out.println(" occurs when"+ne);
+
+        }
+
+    }
+
+
+}
